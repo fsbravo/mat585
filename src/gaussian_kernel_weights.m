f859dc03c6d6959816a2d3913937494edf7c27da
Kyle Gerard Felker <kfelker@princeton.edu>
2015-05-02 15:43:33 -0400

    Reweighted epsilon in gaussian kernel by median of pairwise distances ala Dsilva et al

--- src/gaussian_kernel_weights.m
+++ src/gaussian_kernel_weights.m
@@ -1,17 +1,15 @@
 %%% calculate affinity between each pair of images using a gaussian kernel
 
-function [A] = gaussian_kernel_weights(imgs)
+function [W] = gaussian_kernel_weights(imgs)
     n = size(imgs,3);
-    A = zeros(n);
+    W = zeros(n);
     for i=1:n
         img_i = imgs(:,:,i);
         for j=i:n
             img_j = imgs(:,:,j);
-            difference = norm(img_i(:)-img_j(:))^2;  %this is on the order of 1e6
-            %normalize by average of squared frobenius norms
-            average_norm = (norm(img_i(:))^2 + norm(img_j(:))^2)/2;
-            A(i,j) = exp(-difference/average_norm);
+            distances(i,j) = norm(img_i(:)-img_j(:))^2;  %this is on the order of 1e6
         end
-    end   
-    A = A + triu(A,1)';
+    end
+    distances = distances + triu(distances,1)';
+    W = exp(-distances./median(distances(:)));
 end
