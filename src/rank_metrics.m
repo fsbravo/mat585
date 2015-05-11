169699c9ecd1ce0746f2bf94eaa7a7ad47272ea0
Kyle Gerard Felker <kfelker@princeton.edu>
2015-05-10 20:52:43 -0400

    untranspose 1:range in rank_metric

--- src/rank_metrics.m
+++ src/rank_metrics.m
@@ -4,13 +4,13 @@ function [ errors ] = rank_metrics(calc_rank, range )
 %metrics: correlation coefficient, sum of distances from true rank, sum of
 %squared distances from true rank, max distance from true rank
 
-abs_diff = sum(abs(calc_rank-(1:range)'));
-square_diff = (calc_rank-(1:range)')'*(calc_rank-(1:range)');
-max_diff = max(abs(calc_rank-(1:range)'));
+abs_diff = sum(abs(calc_rank-(1:range)));
+square_diff = (calc_rank-(1:range))'*(calc_rank-(1:range));
+max_diff = max(abs(calc_rank-(1:range)));
 
 %Get correlation coefficient
-p= polyfit((1:range)',calc_rank,1);
-yfit =  polyval(p,(1:range)');
+p= polyfit((1:range),calc_rank,1);
+yfit =  polyval(p,(1:range));
 yresid = calc_rank - yfit; 
 SSresid = sum(yresid.^2);
 SStotal = (length(calc_rank)-1) * var(calc_rank);
